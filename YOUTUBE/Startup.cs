bf3b477ec71c4c4cebd0fbf9cee5e4344c5da467
Vũ Đức Cường <home.phantom.yi@hotmail.com>
2021-07-11 13:34:39 +0700

    aaa

--- YOUTUBE/Startup.cs
+++ YOUTUBE/Startup.cs
@@ -35,10 +35,9 @@ namespace YOUTUBE
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "YOUTUBE v1"));
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "YOUTUBE v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
